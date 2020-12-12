169a44753d31cabd0e00f566f79db9f5e07e12f5
Rikr0 <riky.rm58@gmail.com>
2020-12-11 18:01:34 -0600

    estilo y complementos de la pokedex

--- src/components/Pokedex.vue
+++ src/components/Pokedex.vue
@@ -11,25 +11,113 @@
   >
     <b-container fluid>
       <b-overlay :show="loading" no-wrap></b-overlay>
-      <b-row class="mb-1 text-center" v-if="id != ''">
-        <b-col cols="3">
-          <img :src="`${urlImg}${id}.gif`" class="img-fluid" />
-        </b-col>
-        <b-col cols="3">
-          <img :src="`${urlImg2}${id}.gif`" class="img-fluid" />
-        </b-col>
-      </b-row>
+      <template v-if="id != ''">
+        <b-row class="mb-3 justify-content-center">
+          <b-col cols="3">
+            <img :src="`${urlImg}${id}.gif`" class="img-fluid" />
+          </b-col>
+          <b-col cols="3">
+            <img :src="`${urlImg2}${id}.gif`" class="img-fluid" />
+          </b-col>
+        </b-row>
+        <b-row class="mb-1 justify-content-center">
+          <b-col cols="12">
+            <b-skeleton-table
+              v-if="loading"
+              :rows="5"
+              :columns="1"
+              :table-props="{ bordered: true, striped: true }"
+            ></b-skeleton-table>
+            <b-card no-body v-if="!loading">
+              <b-tabs pills card vertical>
+                <b-tab badge="50" active>
+                  <template #title>
+                    Abilities
+                    <b-badge variant="warning" pill>
+                      {{ pokemon.abilities.length }}
+                    </b-badge>
+                  </template>
+                  <b-card-text class="scroll-list">
+                    <b-list-group>
+                      <b-list-group-item
+                        v-for="(item, index) in pokemon.abilities"
+                        :key="index"
+                      >
+                        {{ item.ability.name }}
+                      </b-list-group-item>
+                    </b-list-group>
+                  </b-card-text>
+                </b-tab>
+                <b-tab>
+                  <template #title>
+                    Moves
+                    <b-badge variant="warning" pill>
+                      {{ pokemon.moves.length }}
+                    </b-badge>
+                  </template>
+                  <b-card-text class="scroll-list">
+                    <b-list-group>
+                      <b-list-group-item
+                        v-for="(item, index) in pokemon.moves"
+                        :key="index"
+                      >
+                        {{ item.move.name }}
+                      </b-list-group-item>
+                    </b-list-group>
+                  </b-card-text>
+                </b-tab>
+                <b-tab>
+                  <template #title>
+                    Stats
+                    <b-badge variant="warning" pill>
+                      {{ pokemon.stats.length }}
+                    </b-badge>
+                  </template>
+                  <b-card-text class="scroll-list">
+                    <b-list-group>
+                      <b-list-group-item
+                        v-for="(item, index) in pokemon.stats"
+                        :key="index"
+                      >
+                        {{ item.stat.name }}
+                      </b-list-group-item>
+                    </b-list-group>
+                  </b-card-text>
+                </b-tab>
+                <b-tab>
+                  <template #title>
+                    Types
+                    <b-badge variant="warning" pill>
+                      {{ pokemon.types.length }}
+                    </b-badge>
+                  </template>
+                  <b-card-text class="scroll-list">
+                    <b-list-group>
+                      <b-list-group-item
+                        v-for="(item, index) in pokemon.types"
+                        :key="index"
+                      >
+                        {{ item.type.name }}
+                      </b-list-group-item>
+                    </b-list-group>
+                  </b-card-text>
+                </b-tab>
+              </b-tabs>
+            </b-card>
+          </b-col>
+        </b-row>
+      </template>
     </b-container>
     <template #modal-footer>
       <div class="w-100">
-        <p class="float-left">Modal Footer Content</p>
+        <p class="float-left"></p>
         <b-button
-          variant="primary"
+          variant="danger"
           size="sm"
           class="float-right"
           @click="show = false"
         >
-          Close
+          Cerrar
         </b-button>
       </div>
     </template>
@@ -40,6 +128,7 @@ import axios from "axios";
 export default {
   name: "Pokedex",
   data: () => ({
+    pokemon: {},
     show: false,
     loading: true,
     urlImg:
@@ -50,38 +139,35 @@ export default {
     name: "",
   }),
   methods: {
-    async getPokemon() {
-      let path = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=60";
+    async getInfo() {
+      let path = `https://pokeapi.co/api/v2/pokemon/${this.id}`;
       try {
         let res = await axios.get(path);
         console.log(res.data);
         if (res.data) {
-          this.pokemons = res.data.results;
-          this.pokemons.forEach((poke, index) => {
-            this.pokemons[index].id = index + 1;
-            this.pokemons[index].status = 0;
-          });
-          this.pokemonsTemp = this.pokemons;
-          setTimeout(() => {
-            this.loading = false;
-          }, 1000);
+          this.pokemon = res.data;
+          this.loading = false;
         } else {
+          this.loading = false;
         }
       } catch (error) {}
     },
     showModal(id, name) {
+      this.loading = true;
       this.id = "";
       this.id = id;
       this.name = name;
       this.show = true;
-      setTimeout(() => {
-        this.loading = false;
-      }, 1000);
+      this.getInfo();
     },
   },
   computed: {},
-  created() {
-    //this.getPokemon();
-  },
+  created() {},
 };
 </script>
+<style>
+.scroll-list {
+  height: 250px;
+  overflow-y: scroll;
+}
+</style>
--- src/views/API-pokemon.vue
+++ src/views/API-pokemon.vue
@@ -1,6 +1,12 @@
 <template>
   <b-container>
     {{ statusLoanding }}
+    <img
+      v-if="pokemonsCap.length != 0"
+      :src="imgs[1]"
+      alt="pokebola"
+      class="img-fluid floating"
+    />
     <b-pagination
       align="center"
       :total-rows="pokemons.length"
@@ -12,26 +18,32 @@
     </b-pagination>
     <b-card-group deck>
       <b-row>
-        <b-col md="4" v-for="(poke, index) in paginador(pokemons)" :key="index">
+        <b-col
+          md="3"
+          class="mb-3"
+          v-for="(poke, index) in paginador(pokemons)"
+          :key="index"
+        >
           <b-overlay :show="loading" no-wrap></b-overlay>
           <b-card
             :title="capitalize(poke.name)"
             :img-src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.id}.png`"
             img-alt="Image"
             img-top
+            border-variant="warning"
           >
             <b-card-text>
-              <b-row class="justify-content-center">
-                <b-col cols="4">
+              <b-row class="justify-content-center my-3">
+                <b-col cols="6">
                   <img
                     :src="imgs[poke.status]"
                     class="img-fluid"
-                    @click="capturar(poke.id)"
+                    @click="pokebola(poke.id, poke.status)"
                   />
                 </b-col>
-                <b-col cols="4">
+                <b-col cols="6">
                   <img
-                    src="../assets/pokedex.jpg"
+                    src="../assets/pokedex.png"
                     class="img-fluid"
                     @click="showModal(poke.id, poke.name)"
                   />
@@ -43,13 +55,22 @@
         </b-col>
       </b-row>
     </b-card-group>
+    <b-pagination
+      align="center"
+      :total-rows="pokemons.length"
+      v-model="paginaActual"
+      :per-page="itemsPorPagina"
+      pills
+      size="lg"
+    >
+    </b-pagination>
     <Pokedex ref="showM" />
   </b-container>
 </template>
 <script>
 import axios from "axios";
-import img0 from "../assets/poke-0.jpg";
-import img1 from "../assets/poke-1.jpg";
+import img0 from "../assets/poke-0.png";
+import img1 from "../assets/poke-1.png";
 import Pokedex from "@/components/Pokedex.vue";
 export default {
   name: "API-pokemon",
@@ -59,9 +80,10 @@ export default {
   data: () => ({
     pokemons: [],
     pokemonsTemp: [],
+    pokemonsCap: [],
     paginaActual: 1,
     paginaActualAux: 1,
-    itemsPorPagina: 6,
+    itemsPorPagina: 12,
     loading: true,
     imgs: [img0, img1],
   }),
@@ -70,13 +92,27 @@ export default {
       let path = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=60";
       try {
         let res = await axios.get(path);
-        console.log(res.data);
+        //console.log(res.data);
         if (res.data) {
           this.pokemons = res.data.results;
           this.pokemons.forEach((poke, index) => {
             this.pokemons[index].id = index + 1;
             this.pokemons[index].status = 0;
           });
+
+          if (localStorage.getItem("pokemons-capturados")) {
+            //saber si hay pokemons guardados en localStorage
+            this.pokemonsCap = JSON.parse(
+              localStorage.getItem("pokemons-capturados")
+            );
+            this.pokemonsCap.forEach((pokeC) => {
+              let index = this.pokemons.findIndex(
+                (poke) => poke.id == pokeC.id
+              );
+              this.pokemons[index].status = 1;
+            });
+          }
+
           this.pokemonsTemp = this.pokemons;
           setTimeout(() => {
             this.loading = false;
@@ -96,13 +132,40 @@ export default {
     capitalize(word) {
       return word[0].toUpperCase() + word.slice(1);
     },
+    pokebola(id, status) {
+      status == 0 ? this.capturar(id) : this.liberar(id);
+    },
     capturar(id) {
       this.pokemons = [];
       let index = this.pokemonsTemp.findIndex((poke) => poke.id === id);
-      console.log(this.pokemonsTemp[index].status);
       this.pokemonsTemp[index].status = 1;
-      console.log(this.pokemonsTemp[index].status);
       this.pokemons = this.pokemonsTemp;
+      this.pokemonsCap.push({
+        id: this.pokemons[index].id,
+        name: this.pokemons[index].name,
+      });
+      localStorage.setItem(
+        "pokemons-capturados",
+        JSON.stringify(this.pokemonsCap)
+      );
+    },
+    liberar(id) {
+      this.pokemons = [];
+      let index = this.pokemonsTemp.findIndex((poke) => poke.id === id);
+      this.pokemonsTemp[index].status = 0;
+      this.pokemons = this.pokemonsTemp;
+
+      let index2 = this.pokemonsCap.findIndex((poke) => poke.id === id);
+
+      this.pokemonsCap.splice(index2, 1);
+      if (this.pokemonsCap.length == 0) {
+        localStorage.removeItem("pokemons-capturados");
+      } else {
+        localStorage.setItem(
+          "pokemons-capturados",
+          JSON.stringify(this.pokemonsCap)
+        );
+      }
     },
     showModal(id, name) {
       this.$refs.showM.showModal(id, name);
@@ -125,3 +188,15 @@ export default {
   },
 };
 </script>
+<style scoped>
+.card-footer {
+  background-color: red;
+}
+.floating {
+  width: 100px;
+  position: fixed;
+  float: right;
+  right: 0px;
+  z-index: 1;
+}
+</style>
